c087ef0b4c5bca496fdb280bdab9e9ae447b59d8
petrovnamm <mariamatosp72@gmail.com>
2018-11-10 11:11:05 -0300

    Mudanças Layout/tela grafico

---
+++ app/src/main/java/com/example/root/appconversao/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.root.appconversao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activity);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this,AppConversao.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
